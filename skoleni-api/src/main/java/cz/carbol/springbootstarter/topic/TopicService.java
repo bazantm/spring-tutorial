7f733babb7f152895c361ce5943ca4312e483e79
Michael Bazant <bazantm@gmail.com>
2021-04-26 16:05:46 +0200

    funkcni pro aktulni verzi Spring

--- skoleni-api/src/main/java/cz/carbol/springbootstarter/topic/TopicService.java
+++ skoleni-api/src/main/java/cz/carbol/springbootstarter/topic/TopicService.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class TopicService {
 
+	@Autowired
+	private TopicRepository topicRepository;
+	
 	private List<Topic> topics = new ArrayList<>(Arrays.asList(
 			new Topic("spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("java", "Core Java", "Core Java Description"),
@@ -16,26 +21,23 @@ public class TopicService {
 			));
 	
 	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll()
+		.forEach(topics::add);
+		
 		return topics;
 	}
 	
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findById(id).orElse(null);
 	}
 
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
 
-	public void updateTopic(String id, Topic topic) {
-		for (int i = 0; i < topics.size(); i++)
-		{
-			Topic t = topics.get(i);
-			if (t.getId().equals(id)) {
-				topics.set(i, topic);
-				return;
-			}
-		}
+	public void updateTopic(Topic topic) {
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
