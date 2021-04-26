ebb5dfc32f59da5425be05ae5040615b2ab50c7d
Michael Bazant <bazantm@gmail.com>
2021-04-26 10:19:58 +0200

    import

---
+++ skoleni-api/src/main/java/cz/carbol/springbootstarter/SkoleniApp.java
@@ -0,0 +1,14 @@
+package cz.carbol.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkoleniApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkoleniApp.class, args);
+
+	}
+
+}
